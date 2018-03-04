6ac139a9355b91718c4b214529b51eb018000b13
olsonj98 <olsonj98@gmail.com>
2018-03-04 13:10:54 -0600

    initial commit, just skeleton code

---
+++ PA1/src/BruteForceSimilarity.java
@@ -0,0 +1,37 @@
+// LEAVE THIS FILE IN THE DEFAULT PACKAGE
+//  (i.e., DO NOT add 'package cs311.pa1;' or similar)
+
+// DO NOT MODIFY THE EXISTING METHOD SIGNATURES
+//  (you may, however, add member fields and additional methods)
+
+// DO NOT INCLUDE LIBRARIES OUTSIDE OF THE JAVA STANDARD LIBRARY
+//  (i.e., you may include java.util.ArrayList etc. here, but not junit, apache commons, google guava, etc.)
+
+/**
+* @author Hugh Potter
+*/
+
+public class BruteForceSimilarity
+{
+	// member fields and other member methods
+
+	public BruteForceSimilarity(String s1, String s2, int sLength)
+	{
+		// implementation
+	}
+
+	public float lengthOfS1()
+	{
+		// implementation
+	}
+
+	public float lengthOfS2()
+	{
+		// implementation
+	}
+
+	public float similarity()
+	{
+		// implementation
+	}
+}
